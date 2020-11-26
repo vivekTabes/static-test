c8df193b944431bcb009e455370dbea653937560
vivek-appscms <vivek@appscms.com>
2020-11-26 08:28:40 +0530

    first

---
+++ _plugins/test.rb
@@ -0,0 +1,3 @@
+File.open( 'vivek-tiwari.md', 'w' ) do |the_file|
+    the_file.puts "--- \n layout: test \n---\n this is only for testing for ongoing development"
+    end
---
+++ testfile.md
@@ -0,0 +1,4 @@
+--- 
+ layout: test21 
+---
+ this is only for testing for ongoing development
