395c07f8f4e6cfee7c913cf7d2b281e2ebdac2ba
vivekTabes <50580024+vivekTabes@users.noreply.github.com>
2020-11-26 19:59:06 +0530

    Update createFile.rb

--- createFile.rb
+++ createFile.rb
@@ -1,4 +1,11 @@
----
-- translate-hindi-to-german
-- translate-hindi-to-polish
-- translate-hindi-to-spanish
+
+require 'yaml'
+file_array = YAML.load(File.read("../createFile.yml"))
+  file_array.each { 
+      |x| 
+      File.open( "#{x}.md", 'w' ) do |the_file|
+          the_file.puts "--- \nlayout: home \nfileName: translate_english_to_hindi \npermalink: /#{x}\n---"
+
+            end
+      }
+    
